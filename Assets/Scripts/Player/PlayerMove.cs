d52de9496ca2467a0dd826caf5431b5a9dedb374
an_yan_yang <yanggogi1423@gmail.com>
2024-09-19 02:29:57 +0900

    20240919 commit

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class playermove : MonoBehaviour
 {
-    private float _speed = 10.0f; //�¿� �̵��ӵ���
-    private float _jumpSpeed = 20.0f; //���� �̵��ӵ���
-    private bool _isGround = true; //���� ���ɿ��� �Ǵ� ����
+    [SerializeField] private float speed = 10.0f; //좌우 이동속도값
+    [SerializeField] private float jumpSpeed = 20.0f; //점프 이동속도값
+    [SerializeField] private bool isGround = true; //점프 가능여부 판단 변수
     public int playerHp = 4;
 
     private Rigidbody2D _rigid;
-    SpriteRenderer spriteRenderer;
-    Animator animator;
-    
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
 
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
 
-	}
+        _rigid.gravityScale = 10.0f;
+    }
 
     void Update()
     {
+	    FacingDirection();
         Move();
         Jump();
     }
@@ -32,42 +33,42 @@ public class playermove : MonoBehaviour
 		
 	}
 
-    //�÷��̾� �̵����� �Լ�
+    //플레이어 이동관련 함수
     void Move()
     {
-		//x�� �������� ������ �ӵ���ŭ �̵�
+		//x축 방향으로 정해진 속도만큼 이동
 		float x = Input.GetAxisRaw("Horizontal");
-		_rigid.velocity = new Vector2(x * _speed, _rigid.velocity.y);
+		_rigid.velocity = new Vector2(x * speed, _rigid.velocity.y);
 
-		//�÷��̾� �¿� ���� ����
-		if (Input.GetButtonDown("Horizontal"))
-		{
-			spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-		}
-
-		//�÷��̾� �̵� �ִϸ��̼�
+		//플레이어 좌우 방향 조절
+		// if (Input.GetButtonDown("Horizontal"))
+		// {
+		// 	spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+		// }
+		
+		//플레이어 이동 애니메이션
 		if (_rigid.velocity.x == 0)
 		{
-			animator.SetBool("isRun", false);
+			_animator.SetBool("isRun", false);
 		}
 		else
 		{
-			animator.SetBool("isRun", true);
+			_animator.SetBool("isRun", true);
 		}
 	}
 
-	//�÷��̾��� �������� �Լ�
+	//플레이어의 점프관련 함수
 	void Jump()
     {
-        //������ �ƴ� ���¿��� space�ٸ� ������ ����
-        if (Input.GetKeyDown(KeyCode.Space)&&_isGround)
+        //공중이 아닌 상태에서 space바를 누르면 점프
+        if (Input.GetKeyDown(KeyCode.Space)&&isGround)
         {
-            _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
-            _isGround = false;
-			animator.SetBool("isJump", true);   
+            _rigid.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            isGround = false;
+            _animator.SetBool("isJump", true);   
 		}
         
-        //Raycast�� �̿��ϴ� ���
+        //Raycast를 이용하는 경우
         /*
         if(_rigid.velocity.y < 0)
         {
@@ -84,29 +85,29 @@ public class playermove : MonoBehaviour
     }
 
 
-    //������ �浹�� üũ�ϴ� �Լ�
+    //땅과의 충돌을 체크하는 함수
     void OnCollisionEnter2D(Collision2D collision)
     {
-        //Ground�±��� ��ü�� ������ ������(�������°� �ƴҽ�)
+        //Ground태그인 물체에 접촉해 있을때(점프상태가 아닐시)
         if (collision.gameObject.CompareTag("Ground"))
         {
-            _isGround=true;
-            animator.SetBool("isJump", false);
+            isGround=true;
+            _animator.SetBool("isJump", false);
         }
-        //���Ϳ��� ���ظ� ���� ���
+        //몬스터에게 피해를 입은 경우
         if(collision.gameObject.tag == "Enemy")
         {
             OnDamaged(collision.transform.position);
         }
     }
-    //�ǰݰ��� �Լ�
+    //피격관련 함수
     void OnDamaged(Vector2 nowPos)
     {
         playerHp--;
         gameObject.layer = 8;
-        spriteRenderer.color = new Color(1, 1, 1, 0.5f);
+        _spriteRenderer.color = new Color(1, 1, 1, 0.5f);
 
-        //�ǰݽ� �̵�
+        //피격시 이동
         int dict = 0;
         if (transform.position.x - nowPos.x > 0)
             dict = 1;
@@ -114,14 +115,20 @@ public class playermove : MonoBehaviour
             dict = -1;
         _rigid.AddForce(new Vector2(dict,1)*1,ForceMode2D.Impulse);
 
-        //�ǰ� �ִ�
-        animator.SetTrigger("isDamaged");
+        //피격 애니
+        _animator.SetTrigger("isDamaged");
 
         Invoke("OffDamaged", 2);
     }
     void OffDamaged()
     {
         gameObject.layer = 7;
-		spriteRenderer.color = new Color(1, 1, 1,1f);
+        _spriteRenderer.color = new Color(1, 1, 1,1f);
 	}
+    //by jd player flip
+    void FacingDirection()
+    {
+	    if(_rigid.velocity.x < 0) _spriteRenderer.flipX = true;//look at the dir of vec
+	    else _spriteRenderer.flipX = false;
+    }
 }
