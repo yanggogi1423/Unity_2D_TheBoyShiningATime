ccb2301a7d73ec38ca6f4c78203736e1a03d98a2
yanggogi1423 <yanggogi1423@gmail.com>
2024-09-30 22:13:31 +0900

    feat Spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private int maxSpawn;
+    [SerializeField] private int curSpawn;
+    [SerializeField] private float spawnRate;
+    
+    public GameObject[] monsterPrefabs;
+    public Transform[] spawnPoints;
+
+    private GameObject _latestSpawn;
+
+    private void Start()
+    {
+        StartCoroutine(MonsterSpwanCoroutine());
+    }
+
+    private void SpawnMonster()
+    {
+        
+        _latestSpawn = GetMonsterRandom();
+        if (_latestSpawn != null)
+        {
+            Instantiate(_latestSpawn, GetPointRandom(), Quaternion.identity); 
+            curSpawn++;
+        }
+        else
+        {
+            Debug.Log("Spawn object is NULL");
+        }
+    }
+
+    private GameObject GetMonsterRandom()
+    {
+        var idx = Random.Range(0, monsterPrefabs.Length);
+        return monsterPrefabs[idx];
+    }
+
+    private Vector3 GetPointRandom()
+    {
+        var idx = Random.Range(0, spawnPoints.Length);
+        return spawnPoints[idx].position;
+    }
+
+    public void UpdateSpawn()
+    {
+        curSpawn--;
+    }
+
+    private IEnumerator MonsterSpwanCoroutine()
+    {
+        while (true)
+        {
+            if (curSpawn < maxSpawn)
+            {
+                SpawnMonster();
+            }
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+}
