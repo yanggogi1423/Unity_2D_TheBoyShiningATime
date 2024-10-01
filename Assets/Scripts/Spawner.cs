ddd6b865067c97b4403e8d1dbb0f694ae5ba9186
yanggogi1423 <yanggogi1423@gmail.com>
2024-10-01 19:43:40 +0900

    fix : Spawner 예외사항 처리

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,9 +18,22 @@ public class Spawner : MonoBehaviour
 
     private GameObject _latestSpawn;
 
+    /*
+     * 추가 예정 기능
+     * 현재는 spawnPoints 전체에 스폰되는 로직임.
+     * 하나의 맵에서 여러 스폰 포인트를 통제하려면 다른 로직을 도입해야 함.
+     * 미래의 나야 부탁해~ 20241001
+     */
     private void Start()
     {
         StartCoroutine(MonsterSpwanCoroutine());
+        
+        //  현재는 Monster Prefab을 수동으로 가져와야 함. 자동으로 폴더에서 Include하는 방법도 있지만 그건 나중에..
+        if (monsterPrefabs.Length == 0)
+        {
+            Debug.LogError("monsterPrefabs가 비어있습니다. Prefab을 수동으로 넣어 주어야 합니다.");
+            enabled = false;
+        }
     }
 
     private void SpawnMonster()
